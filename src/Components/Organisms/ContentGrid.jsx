b89ff91c4adc75b48992707ad336b081d18b90d9
ILikeMintChocolate <qjatjr99@naver.com>
2022-11-09 17:13:38 +0900

    fix: fix grid style and card index

--- src/Components/Organisms/ContentGrid.jsx
+++ src/Components/Organisms/ContentGrid.jsx
@@ -10,13 +10,14 @@ const ContentGridDiv = styled.div `
     left: 50%;
     transform: translateX(-50%);
     row-gap: 30rem;
+    column-gap: 40rem;
 `;
 
 function ContentGrid({data}) {
     if (data != undefined) {
         return (
             <ContentGridDiv>
-                {data.map(element => {
+                {data.map((element, index) => {
                     let year1 = element.release_date || '';
                     let year2 = year1?.slice(0, 4);
                     let desc = `${year2}`
@@ -28,7 +29,7 @@ function ContentGrid({data}) {
                     } catch (error) { }
                     return(
                         <ContentCard id={element.id} title={element.title} desc={desc} score={element.vote_average * 10} 
-                        posterUrl={element.poster_path} index={1} type={'movie'} />
+                        posterUrl={element.poster_path} index={index} type={'movie'} />
                     )
                 })}
             </ContentGridDiv>
